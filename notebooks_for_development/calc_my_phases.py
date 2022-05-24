52e6b635e480275817759fcf58a1b2d9d0020993
Eckhart Spalding <espaldin@nd.edu>
2022-05-24 16:38:21 -0400

    script to compare NDL and my phases, and error in phase

--- notebooks_for_development/calc_my_phases.py
+++ notebooks_for_development/calc_my_phases.py
@@ -1,35 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-# This reads in files of period info, epochs-of-max, and spectra BJDs to 
-# find the phases as the time the spectra were taken 
+# This reads in files of period info, epochs-of-max, and spectra BJDs to
+# find the phases as the time the spectra were taken
 
 # Created 2022 May 23 by E.S.
 
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
-
-
-# In[2]:
-
+import matplotlib.pyplot as plt
 
 df_spectra_epochs = pd.read_csv("./data/spec_bjds.csv")
 df_stellar_periods = pd.read_csv("./data/output_periods_all_stars.csv")
 df_epochs_of_max = pd.read_csv("./data/bjds_epochs_of_max.csv")
 
 
-# In[3]:
-
-
 # make column in each dataframe that will be used to match by star
-
 df_spectra_epochs["star_match"] = df_spectra_epochs["file"]
 df_spectra_epochs["star_match"] = df_spectra_epochs["star_match"].str.replace("__"," ") # underscore of 2
 df_spectra_epochs["star_match"] = df_spectra_epochs["star_match"].str.replace("_"," ") # underscore of 1
@@ -40,17 +26,9 @@ df_epochs_of_max["star_match"] = df_epochs_of_max["star"].str.replace("_"," ")
 df_stellar_periods["star_match"] = df_stellar_periods["star"]
 
 
-# In[4]:
-
-
 # remove some ambiguities
-
 df_epochs_of_max["photo_bjd"] = df_epochs_of_max["bjd"]
 df_spectra_epochs["spec_bjd"] = df_spectra_epochs["bjd"]
-
-
-# In[5]:
-
 
 # combine epochs-of-max and spectral epochs
 result = pd.merge(df_spectra_epochs,
@@ -62,32 +40,28 @@ result2 = pd.merge(result,
                  df_stellar_periods[['star_match','T_final','err_tot']],
                  on='star_match')
 
-
-# In[6]:
-
-
 # find phases
-
 result2["phasemod"] = np.nan
 result2["baseline_time"] = np.subtract(result2["spec_bjd"],result2["photo_bjd"])
+result2["baseline_div_period"] = np.divide(result2["baseline_time"],result2["T_final"])
 
 # for cases where spectra were taken after photometry
+#result2["phasemod"] = np.mod(result2["baseline_time"],result2["T_final"])
+result2["phasemod"] = [t%1. for t in result2["baseline_div_period"]] # intermediate values which still have to be recalculated
+'''
 idx_pos = (result2["baseline_time"] > 0)
 result2["phasemod"].loc[idx_pos] = np.mod(result2["baseline_time"].where(idx_pos),result2["T_final"].where(idx_pos))
 
 # vice versa
 idx_neg = (result2["baseline_time"] < 0)
-result2["phasemod"].loc[idx_neg] = np.subtract(1.,np.mod(result2["baseline_time"].where(idx_neg),result2["T_final"].where(idx_neg)))
-
-
-# In[7]:
-
-
+result2["phasemod"].loc[idx_neg] = np.subtract(1.,
+                                                np.mod(result2["baseline_time"].where(idx_neg),
+                                                        result2["T_final"].where(idx_neg))
+                                                )
+'''
 result3 = result2.sort_values(by="file").reset_index(drop=True)
 
-
-# In[8]:
-
+import ipdb; ipdb.set_trace()
 
 '''
 # fyi print
@@ -97,15 +71,4 @@ for t in range(0,len(result3)):
     print("----")
 '''
 
-
-# In[9]:
-
-
 result3.to_csv("./data/junk.csv", index=False)
-
-
-# In[ ]:
-
-
-
-
---
+++ notebooks_for_development/compare_ndl_my_phases.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# This reads in my and NDL's phase analysis outputs and compares them
+
+# Create 2022 May 28 by E.S.
+
+
+# In[46]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+get_ipython().run_line_magic('matplotlib', 'qt')
+
+
+# In[47]:
+
+
+# read in NDL
+df_ndl_epochs = pd.read_csv("./data/spectra_epochs_lc.csv")
+df_ndl_phases = pd.read_csv("./data/phases_ndl.csv")
+
+# read in mine
+df_mine = pd.read_csv("./data/spectra_my_bjds.csv")
+
+
+# In[48]:
+
+
+# make specific
+df_mine["my_spec_bjd"] = np.subtract(df_mine["bjd"],2400000) # subtract to compare with NDL
+df_mine["my_phase"] = df_mine["phasemod"]
+df_mine["spec_file"] = df_mine["file"] # the original spectrum file name
+df_ndl_epochs["spec_file"] = df_ndl_epochs["filenames"] # the original spectrum file name (no change by NDL)
+df_ndl_epochs["ndl_spec_bjd"] = df_ndl_epochs["bjd"]
+df_ndl_phases["ndl_phase"] = df_ndl_phases["Phases"]
+
+
+# In[49]:
+
+
+# extract the "cur" number (note that "filenames" here refers to NDL's files, not my spectra numbers)
+##df_ndl_epochs["number_cur"] = df_ndl_epochs["filenames"].str.split(".fits").str[-2].str[-2:]#extract('(\d+)')
+# remove redundant rows with "c.fits" in filenames col
+df_ndl_epochs = df_ndl_epochs.loc[df_ndl_epochs["filenames"].str.contains(".c.fits")==False]
+# convert to ints
+df_ndl_epochs["number_cur"] = df_ndl_epochs["#"].astype(int)
+
+# extract the "cur" number from NDL's other table
+df_ndl_phases["number_cur"] = df_ndl_phases["#Name"].str.split("_").str[-1]
+#df_ndl_phases["number_cur"] = df_ndl_phases["#Name"].str.extract('(\d+)')
+df_ndl_phases["number_cur"] = df_ndl_phases["number_cur"].astype(int)
+
+
+# In[50]:
+
+
+# extract the star name
+df_ndl_epochs["star_name"] = df_ndl_epochs["filenames"].str[:6]
+df_ndl_phases["star_name"] = df_ndl_phases["#Name"].str[:6]
+
+
+# In[51]:
+
+
+# merge NDL's tables with each other based on star name and cur number
+
+df_ndl_merged = df_ndl_epochs.merge(df_ndl_phases, on=["star_name","number_cur"], suffixes=(None,"_y"))
+
+
+# In[52]:
+
+
+# match NDL net table to my results by spectrum number (#)
+df_all_merged = df_mine.merge(df_ndl_merged, how='inner', on=["spec_file"])
+
+
+# In[53]:
+
+
+# find NDL time baselines, for checking only
+
+df_all_merged["ndl_time_baselines"] = np.subtract(df_all_merged["Epoch_Max"],df_all_merged["ndl_spec_bjd"])
+df_all_merged["ndl_baseline_div_period"] = np.divide(df_all_merged["ndl_time_baselines"],df_all_merged["period"])
+
+
+# In[54]:
+
+
+# for fyi, find error in phases: multiply error in period by number of cycles in the time baseline
+df_all_merged["error_my_phase"] = np.multiply(np.abs(df_all_merged["baseline_div_period"]),df_all_merged["err_tot"])
+
+
+# In[55]:
+
+
+'''
+# for checking
+plt.clf()
+plt.hist(df_all_merged["error_my_phase"], bins=100)
+plt.show()
+'''
+
+
+# In[56]:
+
+
+'''
+# for checking
+plt.scatter(df_all_merged["my_spec_bjd"],df_all_merged["ndl_spec_bjd"])
+plt.plot([56200,56500],[56200,56500], linestyle=":", color="gray")
+plt.show()
+'''
+
+
+# In[57]:
+
+
+'''
+# for checking
+plt.scatter(df_all_merged["Period"],df_all_merged["T_final"])
+plt.plot([0,1],[0,1], linestyle=":", color="gray")
+plt.show()
+'''
+
+
+# In[59]:
+
+
+# for comparing NDL and my phases, and troubleshooting disagreement
+'''
+plt.clf()
+plt.scatter(df_all_merged["my_phase"],df_all_merged["ndl_phase"])
+
+for i in range(0,len(df_all_merged)):
+
+    plt.annotate(df_all_merged["spec_file"].loc[i],
+                 xy=(df_all_merged["my_phase"].loc[i],df_all_merged["ndl_phase"].loc[i]))
+
+    plt.annotate(np.round(df_all_merged["ndl_baseline_div_period"].loc[i],2),
+                 xy=(df_all_merged["my_phase"].loc[i],df_all_merged["ndl_phase"].loc[i]))
+
+#plt.scatter(np.subtract(1.,df_all_merged["my_phase"]),df_all_merged["ndl_phase"])
+plt.plot([0,1],[0,1], linestyle=":", color="gray")
+plt.show()
+'''
+
+
+# In[60]:
+
+
+df_all_merged.to_csv("./data/junk.csv")
+
+
+# In[ ]:
+
+
+
+
