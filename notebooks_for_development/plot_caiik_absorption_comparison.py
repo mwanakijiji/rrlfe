0828353829ed5c822bad0e8342f03288b3446ae7
Eckhart Spalding <espaldin@nd.edu>
2022-10-31 09:33:34 -0400

    lines btwn min and max

--- notebooks_for_development/plot_caiik_absorption_comparison.py
+++ notebooks_for_development/plot_caiik_absorption_comparison.py
@@ -12,9 +12,11 @@ df.reset_index(inplace=True)
 
 # Draw plot
 plt.figure(figsize=(14,10), dpi= 80)
-plt.scatter(df.max_ism, df.index, s=140, color="red", label="ISM (max)")
+plt.scatter(df.max_ism, df.index, s=140, marker="<", color="red", label="ISM")
 plt.scatter(df.min_caiik_ew, df.index, s=140, marker=">", color="k", label="Measured (min)")
 plt.scatter(df.max_caiik_ew, df.index, s=140, marker="<", color="k", label="Measured (max)")
+#[plt.plot(df.min_caiik_ew, df.max_caiik_ew) for star_name in df.index]
+plt.hlines(y=df.index, xmin=df.min_caiik_ew, xmax=df.max_caiik_ew, color="k", lw=3)
 
 # Decorations
 # Lighten borders
@@ -28,6 +30,6 @@ plt.yticks(df.index, df.name, fontsize=30)
 plt.xlabel("Ca II K EW ($\AA$)", fontsize=30)
 plt.grid(linestyle='--', alpha=0.5)
 plt.xlim(0, 13)
-plt.legend(loc="lower right", fontsize=30)
+#plt.legend(loc="lower right", fontsize=30)
 plt.tight_layout()
 plt.savefig("junk.pdf")
