8d07b9d487465c969ffa60112ba7ed0f7fde1353
Eckhart Spalding <espaldin@nd.edu>
2023-01-19 12:12:44 -0700

    lamost applic

--- pipeline_application_lamost.py
+++ pipeline_application_lamost.py
@@ -25,7 +25,7 @@ step = pipeline.compile_normalization.CompileBkgrnd(module_name="module3")
 # add step to procedure
 test_gen.add_step(step)
 
-
+'''
 # take list of unnormalized empirical spectra, normalize them, and write out
 step = pipeline.create_spec_realizations.CreateSpecRealizationsMain(
     module_name="module4",
@@ -41,23 +41,23 @@ step = pipeline.create_spec_realizations.CreateSpecRealizationsMain(
 
 # add step to procedure
 test_gen.add_step(step)
-'''
+
 # run_robospect on normalized synthetic spectra
 step = pipeline.run_robo.Robo(
     module_name="module5",
     robo_dir_read="../robospect.py/",
-    normzed_spec_dir_read=stem_abs+"rrlfe_io_20221220_sdss_test/realizations_output/norm/final/",
-    robo_output_write=stem_abs+"rrlfe_io_20221220_sdss_test/robospect_output/smo_files/")
+    normzed_spec_dir_read=stem_abs+"src/lamost_20230105_normalized_rays_removed_3911_to_4950_angstr/",
+    robo_output_write=stem_abs+"rrlfe_io_20230105_lamost_test/robospect_output/smo_files/")
 
 # add step to procedure
 test_gen.add_step(step)
-
+'''
 # scrape_ew_from_robo and calculate EWs + err_EW
 step = pipeline.scrape_ew_and_errew.Scraper(
     module_name="module6",
-    input_spec_list_read=stem_abs+"src/sdss_test_20221213.list",
-    robo_output_read=stem_abs+"rrlfe_io_20221220_sdss_test/robospect_output/smo_files/",
-    file_scraped_write=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/all_ew_info.csv")
+    input_spec_list_read=stem_abs+"src/lamost_test_20230105.list",
+    robo_output_read=stem_abs+"rrlfe_io_20230105_lamost_test/robospect_output/smo_files/",
+    file_scraped_write=stem_abs+"rrlfe_io_20230105_lamost_test/ew_products/all_ew_info.csv")
 
 # add step to procedure
 test_gen.add_step(step)
@@ -65,8 +65,8 @@ test_gen.add_step(step)
 # scrape_ew_from_robo and calculate EWs + err_EW
 step = pipeline.scrape_ew_and_errew.QualityCheck(
     module_name="module7",
-    file_scraped_all_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/all_ew_info.csv",
-    file_scraped_good_write=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/ew_info_good_only.csv")
+    file_scraped_all_read=stem_abs+"rrlfe_io_20230105_lamost_test/ew_products/all_ew_info.csv",
+    file_scraped_good_write=stem_abs+"rrlfe_io_20230105_lamost_test/ew_products/ew_info_good_only.csv")
 
 # add step to procedure
 test_gen.add_step(step)
@@ -74,9 +74,9 @@ test_gen.add_step(step)
 # transpose/stack all the data, where each row corresponds to a spectrum
 step = pipeline.scrape_ew_and_errew.StackSpectra(
     module_name="module8",
-    file_ew_data_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/ew_info_good_only.csv",
-    file_restacked_write=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only.csv",
-    input_spec_list_read=stem_abs+"src/sdss_test_20221213.list")
+    file_ew_data_read=stem_abs+"rrlfe_io_20230105_lamost_test/ew_products/ew_info_good_only.csv",
+    file_restacked_write=stem_abs+"rrlfe_io_20230105_lamost_test/ew_products/restacked_ew_info_good_only.csv",
+    input_spec_list_read=stem_abs+"src/lamost_test_20230105.list")
 
 # add step to procedure
 test_gen.add_step(step)
@@ -84,8 +84,8 @@ test_gen.add_step(step)
 # make a net Balmer line from the H-delta and H-gamma lines
 step = pipeline.scrape_ew_and_errew.GenerateNetBalmer(
     module_name="module9",
-    file_restacked_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only.csv",
-    file_ew_net_balmer_write=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only_w_net_balmer.csv")
+    file_restacked_read=stem_abs+"rrlfe_io_20230105_lamost_test/ew_products/restacked_ew_info_good_only.csv",
+    file_ew_net_balmer_write=stem_abs+"rrlfe_io_20230105_lamost_test/ew_products/restacked_ew_info_good_only_w_net_balmer.csv")
 
 # add step to procedure
 test_gen.add_step(step)
@@ -93,8 +93,8 @@ test_gen.add_step(step)
 # add errors from noise-churning (obsolete)
 step = pipeline.scrape_ew_and_errew.GenerateAddlEwErrors(
     module_name="module10",
-    ew_data_restacked_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only_w_net_balmer.csv",
-    ew_data_w_net_balmer_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only_w_net_balmer_errors.csv")
+    ew_data_restacked_read=stem_abs+"rrlfe_io_20230105_lamost_test/ew_products/restacked_ew_info_good_only_w_net_balmer.csv",
+    ew_data_w_net_balmer_read=stem_abs+"rrlfe_io_20230105_lamost_test/ew_products/restacked_ew_info_good_only_w_net_balmer_errors.csv")
 
 
 # add step to procedure
@@ -102,12 +102,12 @@ test_gen.add_step(step)
 
 step = pipeline.find_feh.FehRetrieval(
     module_name="module11",
-    file_good_ew_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only_w_net_balmer_errors.csv",
+    file_good_ew_read=stem_abs+"rrlfe_io_20230105_lamost_test/ew_products/restacked_ew_info_good_only_w_net_balmer_errors.csv",
     file_calib_read=stem_abs+"src/calib_solution_20220623_1.fits",
-    dir_retrievals_write=stem_abs+"rrlfe_io_20221220_sdss_test/bin/pickled_info/",
-    file_retrievals_write=stem_abs+"rrlfe_io_20221220_sdss_test/bin/retrieved_vals.csv")
+    dir_retrievals_write=stem_abs+"rrlfe_io_20230105_lamost_test/bin/pickled_info/",
+    file_retrievals_write=stem_abs+"rrlfe_io_20230105_lamost_test/bin/retrieved_vals.csv")
 
 # add step to procedure
 test_gen.add_step(step)
-'''
+
 test_gen.run()
