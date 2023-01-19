995d7fc6012c103ac0b912cc1b94cdddc0f72c4b
Eckhart Spalding <espaldin@nd.edu>
2023-01-19 13:46:49 -0700

    rest of pipeline on planetfinder

--- pipeline_application_lamost.py
+++ pipeline_application_lamost.py
@@ -42,7 +42,7 @@ step = pipeline.create_spec_realizations.CreateSpecRealizationsMain(
 
 # add step to procedure
 test_gen.add_step(step)
-'''
+
 # run_robospect on normalized synthetic spectra
 step = pipeline.run_robo.Robo(
     module_name="module5",
@@ -110,5 +110,5 @@ step = pipeline.find_feh.FehRetrieval(
 
 # add step to procedure
 test_gen.add_step(step)
-'''
+
 test_gen.run()
