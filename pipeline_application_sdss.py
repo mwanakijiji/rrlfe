84ceebb8123747cb0a05e9e691e00cf412675f1c
Eckhart Spalding <espaldin@nd.edu>
2023-05-14 15:21:34 +1000

    sdss

--- pipeline_application_sdss.py
+++ pipeline_application_sdss.py
@@ -1,7 +1,9 @@
 import high_level_application_accordion as pipeline
 
 # absolute stem of repo; needed to make dirs if they don't exist
-stem_abs = "/Users/bandari/Documents/git.repos/rrlfe/"
+stem_abs = "/suphys/espa3021/Documents/git.repos/rrlfe/"
+#stem_abs = "/Users/bandari/Documents/git.repos/rrlfe/"
+#stem_abs = "/home/prunelle/rrlfe/"
 
 # instantiate object that will contain the series of reduction steps
 test_gen = pipeline.ApplyCalib() ## ## need to let this set config file being read in (currently in __init__)
@@ -19,21 +21,24 @@ step = pipeline.MakeDirsConfig(module_name="module2")
 # add step to procedure
 test_gen.add_step(step)
 '''
+
 # compile the C spectral normalization script
-step = pipeline.compile_normalization.CompileBkgrnd(module_name="module3")
+step = pipeline.compile_normalization.CompileBkgrnd(
+    module_name="module3",
+    cc_bkgrnd_dir=stem_abs+"src/")
 
 # add step to procedure
 test_gen.add_step(step)
 
-'''
 # take list of unnormalized empirical spectra, normalize them, and write out
 step = pipeline.create_spec_realizations.CreateSpecRealizationsMain(
     module_name="module4",
-    input_spec_list_read=stem_abs+"src/sdss_test_20221213.list",
+    cc_bkgrnd_dir=stem_abs+"src/",
+    input_spec_list_read=stem_abs+"src/sdss_20221213_cosmic_rays_removed_automated_3900_to_5299_angstr.list",
     unnorm_spectra_dir_read=stem_abs+"src/sdss_20221213_cosmic_rays_removed_automated_3900_to_5299_angstr/",
-    unnorm_noise_churned_spectra_dir_read=stem_abs+"src/realizations_output/",
-    bkgrnd_output_dir_write=stem_abs+"rrlfe_io_20221220_sdss_test_2/realizations_output/norm/",
-    final_spec_dir_write=stem_abs+"rrlfe_io_20221220_sdss_test_2/realizations_output/norm/final/",
+    unnorm_noise_churned_spectra_dir_read=stem_abs+"rrlfe_io_20230514_sdss/realizations_output/",
+    bkgrnd_output_dir_write=stem_abs+"rrlfe_io_20230514_sdss/realizations_output/norm/",
+    final_spec_dir_write=stem_abs+"rrlfe_io_20230514_sdss/realizations_output/norm/final/",
     noise_level=0.0,
     spec_file_type="ascii.no_header",
     number_specs=1,
@@ -46,8 +51,8 @@ test_gen.add_step(step)
 step = pipeline.run_robo.Robo(
     module_name="module5",
     robo_dir_read="../robospect.py/",
-    normzed_spec_dir_read=stem_abs+"rrlfe_io_20221220_sdss_test/realizations_output/norm/final/",
-    robo_output_write=stem_abs+"rrlfe_io_20221220_sdss_test/robospect_output/smo_files/")
+    normzed_spec_dir_read=stem_abs+"rrlfe_io_20230514_sdss/realizations_output/norm/final/",
+    robo_output_write=stem_abs+"rrlfe_io_20230514_sdss/robospect_output/smo_files/")
 
 # add step to procedure
 test_gen.add_step(step)
@@ -55,9 +60,9 @@ test_gen.add_step(step)
 # scrape_ew_from_robo and calculate EWs + err_EW
 step = pipeline.scrape_ew_and_errew.Scraper(
     module_name="module6",
-    input_spec_list_read=stem_abs+"src/sdss_test_20221213.list",
-    robo_output_read=stem_abs+"rrlfe_io_20221220_sdss_test/robospect_output/smo_files/",
-    file_scraped_write=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/all_ew_info.csv")
+    input_spec_list_read=stem_abs+"src/sdss_20221213_cosmic_rays_removed_automated_3900_to_5299_angstr.list",
+    robo_output_read=stem_abs+"rrlfe_io_20230514_sdss/robospect_output/smo_files/",
+    file_scraped_write=stem_abs+"rrlfe_io_20230514_sdss/ew_products/all_ew_info.csv")
 
 # add step to procedure
 test_gen.add_step(step)
@@ -65,8 +70,8 @@ test_gen.add_step(step)
 # scrape_ew_from_robo and calculate EWs + err_EW
 step = pipeline.scrape_ew_and_errew.QualityCheck(
     module_name="module7",
-    file_scraped_all_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/all_ew_info.csv",
-    file_scraped_good_write=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/ew_info_good_only.csv")
+    file_scraped_all_read=stem_abs+"rrlfe_io_20230514_sdss/ew_products/all_ew_info.csv",
+    file_scraped_good_write=stem_abs+"rrlfe_io_20230514_sdss/ew_products/ew_info_good_only.csv")
 
 # add step to procedure
 test_gen.add_step(step)
@@ -74,9 +79,9 @@ test_gen.add_step(step)
 # transpose/stack all the data, where each row corresponds to a spectrum
 step = pipeline.scrape_ew_and_errew.StackSpectra(
     module_name="module8",
-    file_ew_data_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/ew_info_good_only.csv",
-    file_restacked_write=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only.csv",
-    input_spec_list_read=stem_abs+"src/sdss_test_20221213.list")
+    file_ew_data_read=stem_abs+"rrlfe_io_20230514_sdss/ew_products/ew_info_good_only.csv",
+    file_restacked_write=stem_abs+"rrlfe_io_20230514_sdss/ew_products/restacked_ew_info_good_only.csv",
+    input_spec_list_read=stem_abs+"src/sdss_20221213_cosmic_rays_removed_automated_3900_to_5299_angstr.list")
 
 # add step to procedure
 test_gen.add_step(step)
@@ -84,8 +89,8 @@ test_gen.add_step(step)
 # make a net Balmer line from the H-delta and H-gamma lines
 step = pipeline.scrape_ew_and_errew.GenerateNetBalmer(
     module_name="module9",
-    file_restacked_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only.csv",
-    file_ew_net_balmer_write=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only_w_net_balmer.csv")
+    file_restacked_read=stem_abs+"rrlfe_io_20230514_sdss/ew_products/restacked_ew_info_good_only.csv",
+    file_ew_net_balmer_write=stem_abs+"rrlfe_io_20230514_sdss/ew_products/restacked_ew_info_good_only_w_net_balmer.csv")
 
 # add step to procedure
 test_gen.add_step(step)
@@ -93,33 +98,32 @@ test_gen.add_step(step)
 # add errors from noise-churning (obsolete)
 step = pipeline.scrape_ew_and_errew.GenerateAddlEwErrors(
     module_name="module10",
-    ew_data_restacked_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only_w_net_balmer.csv",
-    ew_data_w_net_balmer_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only_w_net_balmer_errors.csv")
+    ew_data_restacked_read=stem_abs+"rrlfe_io_20230514_sdss/ew_products/restacked_ew_info_good_only_w_net_balmer.csv",
+    ew_data_w_net_balmer_read=stem_abs+"rrlfe_io_20230514_sdss/ew_products/restacked_ew_info_good_only_w_net_balmer_errors.csv")
 
 
 # add step to procedure
 test_gen.add_step(step)
-'''
+
 step = pipeline.find_feh.FehRetrieval(
     module_name="module11",
-    file_good_ew_read=stem_abs+"rrlfe_io_20221220_sdss_test/ew_products/restacked_ew_info_good_only_w_net_balmer_errors.csv",
-    file_calib_read=stem_abs+"src/calib_solution_20220623_1.fits",
-    dir_retrievals_write=stem_abs+"rrlfe_io_20221220_sdss_test/bin/pickled_info/",
-    file_retrievals_write=stem_abs+"rrlfe_io_20221220_sdss_test/bin/retrieved_vals_parallel_test.csv")
+    file_good_ew_read=stem_abs+"rrlfe_io_20230514_sdss/ew_products/restacked_ew_info_good_only_w_net_balmer_errors.csv",
+    file_calib_read=stem_abs+"rrlfe_io_20230507_synthetic/bin/calib_solution_20230507.fits",
+    dir_retrievals_write=stem_abs+"rrlfe_io_20230514_sdss/bin/pickled_info/",
+    file_retrievals_write=stem_abs+"rrlfe_io_20230514_sdss/bin/retrieved_vals.csv")
 
 # add step to procedure
 test_gen.add_step(step)
-'''
-# apply the raw calibration to the McD star EW data, and find the correction based on them
-# this requires a separate application script to have run on the McD data; it's too complicated to build it in here; TBD later
+
+# apply final correction
 step = pipeline.final_corrxn.ApplyCorrxn(
     module_name="module16",
-    file_name_basis_raw_retrieved_fehs=stem_abs+"rrlfe_io_20221220_sdss_test/bin/retrieved_vals.csv", # retrieved McD Fe/H values based on raw rrlfe calibration
-    soln_fits_name=stem_abs+"rrlfe_io_red/bin/junk_calib_solution.fits", # raw calibration which is applied, and to which corrxn is appended to
-    file_name_corrected_retrieved_fehs_write=stem_abs+"rrlfe_io_20221220_sdss_test/bin/retrieved_vals_corrected.csv"
+    file_name_basis_raw_retrieved_fehs=stem_abs+"rrlfe_io_20230514_sdss/bin/retrieved_vals.csv", # retrieved McD Fe/H values based on raw rrlfe calibration
+    soln_fits_name=stem_abs+"rrlfe_io_20230507_synthetic/bin/calib_solution_20230507.fits", # calibration file which includes correction info in the header
+    file_name_corrected_retrieved_fehs_write=stem_abs+"rrlfe_io_20230514_sdss/bin/retrieved_vals_corrected.csv" # mapped high-res literature Fe/H values for McD stars
 )
 
 # add step to procedure
 test_gen.add_step(step)
-'''
+
 test_gen.run()
