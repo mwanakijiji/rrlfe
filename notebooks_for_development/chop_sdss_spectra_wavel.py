ccd3f4098a6e23fb1122f4231c72f3b33c245196
Eckhart Spalding <espaldin@nd.edu>
2023-06-04 19:25:10 +1000

    chop script

--- notebooks_for_development/chop_sdss_spectra_wavel.py
+++ notebooks_for_development/chop_sdss_spectra_wavel.py
@@ -31,31 +31,8 @@ for file_num in range(0,len(file_list_read)):
     df = pd.read_csv(file_list_read[file_num])
     # wavel,flux_net,noise_net
 
-    # indices of the desired wavelength range
-    import ipdb; ipdb.set_trace()
-    idx = df.where( np.logical_and(df['wave'] >= 3911., df['wave'] <= 4950.) )
+    # chop by wavelength and drop old indices
+    df = df.where( np.logical_and(df['wavel'] >= 3911., df['wavel'] <= 4950.) ).dropna().reset_index(drop=True)
     #idx = np.logical_and(hdu[0].data[2,:] >= 3911., hdu[0].data[2,:] <= 4950.) # McD data chopped to this
-
-    # initialize new DataFrame to hold spectrum
-    df_spec_this = pd.DataFrame(hdu[0].data[2,idx], columns=["wavel"])
-    df_spec_this["flux"] = hdu[0].data[0,idx]
-    df_spec_this["err_flux"] = np.divide(hdu[0].data[0,:],1./np.sqrt(hdu[0].data[1,:]))[idx]
-    #df_spec_this["err_flux"] = hdu[0].data[0,idx]
-
-    # add S/N to table AFTER having chopped the spectra
-    df_ra_dec["emp_snr"].loc[file_num] = np.median(np.divide(hdu[0].data[0,idx],1./np.sqrt(hdu[0].data[1,idx])))
-
-    # write out spectrum
-    df_spec_this.to_csv(dir_write + os.path.basename(file_list_read[file_num]).split(".")[0]+".dat",
+    df.to_csv(dir_write + os.path.basename(file_list_read[file_num]).split(".")[0]+".dat",
                         sep=" ", index=False, header=False)
-
-    # make FYI plot
-    '''
-    plt.clf()
-    plt.plot(df_spec_this["wavel"], df_spec_this["flux"])
-    plt.savefig("junk" + os.path.basename(file_list_read[file_num]).split(".")[0]+".png")
-    '''
-
-# write out list of file info for cross-matching
-# file_name: lamost_li_file_info.csv
-df_ra_dec.to_csv(stem + "notebooks_for_development/data/junk.csv", index=False)
