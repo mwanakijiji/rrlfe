afe93656ebc4afe4a38242c71d955523b3f950a0
mwanakijiji <eckhart.spalding@sydney.edu.au>
2024-01-03 12:06:05 -0700

    8-param unit test

--- tests/test_run_emcee.py
+++ tests/test_run_emcee.py
@@ -169,9 +169,7 @@ def test_LM_fit():
     coeffs_8_test_fitted = least_squares(run_emcee.K_residual, x0=[1,1,1,1,1,1,1,1],
                                         args=(test_balmer_ew_array, test_feh_array, test_caiik_ew_array_8),
                                         method="lm")
-    print("coeffs8")
-    print(coeffs_8_test_fitted.x)
-    print(coeffs_8_test)
+
     # assert that the fitted coefficients are effectively the same as the ones
     # which were chosen for generating the test data
     assert np.allclose(
@@ -179,14 +177,12 @@ def test_LM_fit():
                         np.round(coeffs_4_test, decimals=1),
                         atol=0.2
                         )
-    '''
-    ## ## to do: make the coeffs_8_test_fitted and coeffs_8_test self-consistent
     assert np.allclose(
                         np.round(coeffs_8_test_fitted.x, decimals=1),
                         np.round(coeffs_8_test, decimals=1),
                         atol=0.2
                         )
-    '''
+
 
 
 def test_sigma_Km_sqd():
