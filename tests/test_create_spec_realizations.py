db4623e3eb2a40aef38f3f8a97581e5df4cca945
Eckhart Spalding <eckhart.spalding@sydney.edu.au>
2024-03-31 11:02:36 +1100

    test

--- tests/test_create_spec_realizations.py
+++ tests/test_create_spec_realizations.py
@@ -71,6 +71,8 @@ def test_calc_noise():
 def test_read_spec():
 
     # ascii format
+    print('current_gen')
+    print((os.path.dirname(__file__))
 
     print('conf_gen')
     print(os.path.join(os.path.dirname(__file__), '../conf', 'config_gen.ini'))
