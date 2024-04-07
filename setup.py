1324525efdf46d57d7fe42c407836a95cacc7337
Eckhart Spalding <eckhart.spalding@sydney.edu.au>
2024-04-07 14:45:56 +1000

    setup file updates

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ from setuptools import setup, find_packages
 long_description = "For determining metallicities of RR Lyraes from low-res spectroscopy see `here <https://rrlfe.readthedocs.io>`__ for more info"
 
 setup(name="rrlfe",
-      version="0.1.13",
+      version="1.0",
       description="For finding FeH from low-res survey spectra of RR Lyrae stars",
       long_description=long_description,
       author="Eckhart Spalding, Ron Wilhelm, Nathan De Lee, Kenneth Carrell",
       author_email="eckhart.spalding@sydney.edu.au",
       url="https://github.com/mwanakijiji/rrlfe",
-      download_url="https://github.com/mwanakijiji/rrlfe/archive/refs/tags/v_0.0.12.tar.gz",
+      download_url="https://github.com/mwanakijiji/rrlfe/archive/refs/tags/v_1.0.tar.gz",
       packages=find_packages(exclude=["rrlfe/DO_NOT_USE_modules", "rrlfe/test_bin/*.fits"]),
       include_package_data=True
       )
